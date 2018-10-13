29bd1bb9c4b846e7e0db690f58a80c7776ea0737
hamscratch <hamscratch@hamscratch-2.local>
2018-10-13 19:44:38 -0400

    added some more combat responses

--- making_classes.php
+++ making_classes.php
@@ -20,6 +20,7 @@ class Character {
 class Hero extends Character {
 
 	public function __construct() {
+		parent::__construct();
 		$this->name = 'Talonic';
 		$this->race = 'Half-Elf';
 		$this->class = 'Ranger';
@@ -67,6 +68,7 @@ class NPC extends Character {
 
 
 	public function __construct() {
+		parent::__construct();
 		$this->name = $this->thingPicker(self::NAMES);
 		$this->race = $this->thingPicker(self::RACES);
 		$this->class = $this->thingPicker(self::CLASSES);
@@ -102,30 +104,60 @@ class NPC extends Character {
 class Actions {
 
 	const ATTACK_RESPONSES = [
-	'hit' => 'You hit your target',
-	'crit_hit' => 'You crush your enemy for a lot of damage',
-	'miss' => 'You missed your target',
+	'hit' => "You swing and hit %s for %s damage.",
+	'crit_hit' => 'You crush your enemy for a lot of damage.',
+	'miss' => 'You missed your target.',
 	'crit_miss' => 'You miss so bad your weapon laughs at you',
+	];
+
+	const DEFENSE_RESPONSES = [
+	'hit' => "%s was hit for %s damage and now has %s hit points left.",
+	'miss' => "%s has %s hit points left.",
+	'dead' => "%s has been slain by %s",
 	];
 
 	public function attack($attacker, $defender) {
 		$attack_roll = rand(1, 6);
-		$ac_check = $attacker[$this->str] + $attack_roll;
+		$ac_check = $attacker->str + $attack_roll;
+		$defense_ac = $defender->ac;
+		$hp_result = $defender->hp - $ac_check;
+		$attack_name = "{$attacker->name} the {$attacker->class}";
+		$defender_name = "{$defender->name} the {$defender->class}";
+		$defender_hp = $defender->hp;
 
-		if ($ac_check > $defender[$this->ac]) {
+		if ($ac_check > $defense_ac) {
 			$result = self::ATTACK_RESPONSES['hit'];
 		} else {
 			$result = self::ATTACK_RESPONSES['miss'];
 		}
 
-		return $result;
+		$attack_message = sprintf($result, $defender_name, $ac_check);
+
+		if ($result == self::ATTACK_RESPONSES['hit'] ) {
+			$defender_text = self::DEFENSE_RESPONSES['hit'];
+		} else {
+			$defender_text = self::DEFENSE_RESPONSES['miss'];
+		}
+		
+		if ($defender_text == self::DEFENSE_RESPONSES['hit']) {
+			$d_text = sprintf(self::DEFENSE_RESPONSES['hit'], $defender_name, $ac_check, $hp_result);
+				if ($d_text = sprintf(self::DEFENSE_RESPONSES['hit'], $defender_name, $ac_check, $hp_result)) {
+						$d_text = sprintf(self::DEFENSE_RESPONSES['dead'], $defender_name, $attack_name);
+				}
+		}
+
+		if ($defender_text == self::DEFENSE_RESPONSES['miss']) {
+			$d_text = sprintf(self::DEFENSE_RESPONSES['miss'], $defender_name, $defender_hp);
+		}
+		
+		return $attack_message . "\n" . $d_text . "\n";
 	}
 
 	public function defend($defender) {
 		$defend_roll = rand(1, 6);
 		$new_ac = $defender->ac + $defend_roll;
 
-		$message = "Your defense has been boosted by {$defend_roll} and is now {$new_ac} \n";
+		$message = "Your defense has been boosted by {$defend_roll} and is now {$new_ac}";
 	}
 
 	public function firstAid($character) {
@@ -142,7 +174,8 @@ $hero->printInventoryList();
 
 $villain->characterInfo();
 
-$hero->actions->attack($hero, $villain);
+$attack = $hero->actions->attack($hero, $villain);
+echo $attack;
 
 
 
