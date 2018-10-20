324e1c0ed6b803490b132243611c881038e0936d
hamscratch <hamscratch@hamscratch-2.local>
2018-10-19 23:16:06 -0400

    added lots of stuff. kill me

--- making_classes.php
+++ making_classes.php
@@ -9,6 +9,7 @@ class Character {
 }
 
 class Stats {
+
 	public $name;
 	public $race;
 	public $class;
@@ -18,6 +19,7 @@ class Stats {
 	public $inventory;
 }
 class Hero extends Character {
+	
 	public function __construct() {
 		parent::__construct();
 		$this->stats->name = 'Talonic';
@@ -27,12 +29,13 @@ class Hero extends Character {
 		$this->stats->ac = 15;
 		$this->stats->str = 8;
 		$this->stats->inventory = [
-			'Melee Weapon' => 'Bastard Sword',
-			'Ranged Weapon' => 'Short Bow',
+			'Melee Weapon' => 'Short Sword',
+			'Ranged Weapon' => 'Long Bow',
 			'First Aid' => 'Bandages',
 			'Ammo' => 'Arrows',
 			];
 	}
+
 	public function printInventoryList () {
 		$inventory = $this->stats->inventory;
 		$contents = [];
@@ -43,6 +46,7 @@ class Hero extends Character {
 		$message = "{$this->stats->name}'s Inventory: \n" . $backpack . "\n";
 		echo $message . "\n";
 	}
+
 	public function characterInfo() {
 		echo "Name: {$this->stats->name} \n" . 
 				   "Race: {$this->stats->race} \n" . 
@@ -54,9 +58,10 @@ class Hero extends Character {
 }
 class NPC extends Character {
 	
-	const NAMES = ['Bilgebottom', 'Gorbash', 'Mordok', 'Terrorfin'];
+	const NAMES = ['Bilge', 'Gorbash', 'Mordok', 'Draxiz', 'Innoruuk', 'Lanys', 'Mooto', 'Treskar'];
 	const RACES = ['Orc', 'Skeleton', 'Gnoll', 'Ghoul'];
 	const CLASSES = ['Grunt', 'Warrior', 'Sorcerer'];
+
 	public function __construct() {
 		parent::__construct();
 		$this->name = $this->thingPicker(self::NAMES);
@@ -66,15 +71,18 @@ class NPC extends Character {
 		$this->ac = $this->numberPicker(8, 12);
 		$this->str = $this->numberPicker(4, 8);
 	}
+
 	public function numberPicker($num1, $num2) {
 		$result = rand($num1, $num2);
 		return $result;
 	}
+
 	public function thingPicker($type) {
 		$index = array_rand($type);
 		$result = $type[$index];
 		return $result;
 	}
+
 	public function characterInfo() {
 		echo "Name: {$this->name} \n" . 
 				   "Race: {$this->race} \n" . 
@@ -86,18 +94,47 @@ class NPC extends Character {
 }
 
 class Items {
-	const ONE_HANDED_WEAPONS = [
-		'Short Sword' => rand(1, 6),
-		'Bastard Sword' => rand(1, 8),
-		'Long Sword' => rand(1, 10), 
+
+	const WEAPONS = [
+		'Short Sword' => 8,
+		'Long Bow' => 10,
+		'Arrows' => 0,
 	];
 
-	public function getWeapon() {
+	const ARMOR = [
+		'Cloth Armor' => 2,
+		'Leather Armor' => 4,
+		'Chainmail Armon' => 6,
+	];
 
+	const POTIONS = [
+		'Health' => 6,
+		'Attack' => 6,
+		'Defense' => 6,
+	];
+
+	static function getWeapon($type) {
+		if ($type === 'Melee') {
+			$weapon = self::WEAPONS['Short Sword'];
+		} else if ($type === 'Ranged') {
+			$weapon = self::WEAPONS['Long Bow'];
+		} else {
+			echo "Error: Not a valid entry. Check your spelling!";
+		}
+		return $weapon;
 	}
 
-	public function getArmor() {
-		
+	static function getPotion($type) {
+		if ($type === 'Health') {
+			$potion = self::POTIONS['Health'];
+		} else if ($type === 'Attack') {
+			$potion = self::POTIONS['Attack'];
+		} else if ($type === 'Defense') {
+			$potion = self::POTIONS['Defense'];
+		} else {
+			echo "Error: Not a valid entry. Check your spelling!";
+		}
+		return $potion;
 	}
 }
 
@@ -107,8 +144,9 @@ class Actions {
 	'hit' => "You swing and hit %s for %s damage.",
 	'crit_hit' => 'You crush your enemy for a lot of damage.',
 	'miss' => 'You missed your target.',
-	'crit_miss' => 'You miss so bad your weapon laughs at you',
+	'crit_miss' => 'You miss so bad your weapon laughs at you.',
 	];
+
 	const DEFENSE_RESPONSES = [
 	'hit' => "%s was hit for %s damage and now has %s hit points left.",
 	'miss' => "%s has %s hit points left.",
@@ -132,16 +170,19 @@ class Actions {
 			return $this->stats_ref->ac;
 		} else if ($stat_string === "str") {
 			return $this->stats_ref->str;
+		} else if ($stat_string === 'inventory') {
+			return $this->stats_ref->inventory;
 		} else {
 			echo "Error: Not a valid entry. Check your spelling!";
 		}
 	}
+
 	public function attack($defender) {
-		$attack_roll = rand(1, 6);
-		$ac_check = $this->getStat("str") + $attack_roll;
+		$weapon_damage = rand(1, Items::getWeapon('Melee'));
+		$ac_check = $this->getStat("str") + rand(1, 6);
 		$defense_ac = $defender->ac;
-		$hp_result = $defender->hp - $ac_check;
-		$attack_name = "{$this->getStat("name")} the {$this->getStat("class")}";
+		$hp_result = $defender->hp - $weapon_damage;
+		$attacker_name = "{$this->getStat("name")} the {$this->getStat("class")}";
 		$defender_name = "{$defender->name} the {$defender->class}";
 		$defender_hp = $defender->hp;
 
@@ -150,7 +191,9 @@ class Actions {
 		} else {
 			$result = self::ATTACK_RESPONSES['miss'];
 		}
-		$attack_message = sprintf($result, $defender_name, $ac_check);
+
+		$attack_message = sprintf($result, $defender_name, $weapon_damage);
+
 		if ($result == self::ATTACK_RESPONSES['hit'] ) {
 			$defender_text = self::DEFENSE_RESPONSES['hit'];
 		} else {
@@ -158,9 +201,9 @@ class Actions {
 		}
 		
 		if ($defender_text == self::DEFENSE_RESPONSES['hit']) {
-			$d_text = sprintf(self::DEFENSE_RESPONSES['hit'], $defender_name, $ac_check, $hp_result);
-				if ($ac_check > $defender_hp) {
-						$d_text = sprintf(self::DEFENSE_RESPONSES['dead'], $defender_name, $attack_name);
+			$d_text = sprintf(self::DEFENSE_RESPONSES['hit'], $defender_name, $weapon_damage, $hp_result);
+				if ($weapon_damage > $defender_hp) {
+						$d_text = sprintf(self::DEFENSE_RESPONSES['dead'], $defender_name, $attacker_name);
 				}
 		}
 		if ($defender_text == self::DEFENSE_RESPONSES['miss']) {
@@ -169,15 +212,18 @@ class Actions {
 		
 		return $attack_message . "\n" . $d_text . "\n";
 	}
+
 	public function defend($defender) {
 		$defend_roll = rand(1, 6);
 		$new_ac = $defender->ac + $defend_roll;
 		$message = "Your defense has been boosted by {$defend_roll} and is now {$new_ac}";
 	}
+
 	public function firstAid($character) {
 		$type = $character[$this->inventory]['firstAid'];
 	}
 }
+
 $hero = new Hero;
 $villain = new NPC;
 //var_dump($hero);
